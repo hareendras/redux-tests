918767fc5b2402cfdec1c84cb7c79902645a5567
Hareendra Seneviratne <hareendras@millenniumitesp.com>
2018-03-12 07:24:51 +0530

    Initial Commit

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,17 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
+import { renderComponent, expect} from '../test_helper';
+import  App  from '../../src/components/app'
 
-describe('App' , () => {
-  let component;
-
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
+// use describe to group together similar tetsts
+describe('App',()=>{
+// use 'it' to test a single attribute of a terget
+  it('Shows the correct text',()=>{
+    //create an instance of app
+    const component = renderComponent(App);
+    // use expect to make an assertion about a target
+    expect(component).to.contain('React simple starter');
+  })
 });
+
+
+
+
