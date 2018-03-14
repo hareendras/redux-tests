44e80c6cdcf1729e092e6d1a0bf5a9ae5bf893d8
Hareendra Seneviratne <hareendras@millenniumitesp.com>
2018-03-14 11:59:05 +0530

    done

--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -1,10 +1,14 @@
 import { expect } from "../test_helper";
-import commentReducer from "../../src/reducers/commnets";
-import SAVE_COMMENT from "../../src/actions/types";
+import commentReducer from "../../src/reducers/comments";
+import { SAVE_COMMENT } from "../../src/actions/types";
 
 describe("Comments Reducer", () => {
   it("handles action with unknows type", () => {
-    expect(commentReducer()).to.be.instanceof(Array);
+    expect(commentReducer(undefined, {})).to.be.instanceof(Array);
   });
-  it("handles action of type SAVE_COMMENT", () => {});
+  it("handles action of type SAVE_COMMENT", () => {
+    const action = { type: SAVE_COMMENT, payload: "new comment" };
+    //console.log(commentReducer([],action));
+    expect(commentReducer([], action)).to.eql(["new comment"]);
+  });
 });
